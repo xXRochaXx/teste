c278b863abedec933f7a849f84255177c8d6c61a
Renan Pigato <renan.silva@dbseller.com.br>
2019-05-10 14:09:37 -0300

    Conflitos na view

--- views/agenda.php
+++ views/agenda.php
@@ -30,12 +30,42 @@ if(!empty($_GET['urgencias'])) {
 }
 
 $urgencias = Urgencia::getInstance()->order('id', 'desc')->getAll(3);
-$melhorias = Melhoria::getInstance()->order('prazo_legal')->order('prazo_acordado')->order('gut', 'desc')->order('id')->filtrarPorUrgencia([$urgenciasSelecionadas], ['*', '(coalesce(gravidade, 1) * coalesce(urgencia, 1) * coalesce(tendencia, 1)) as gut']);
+$melhoriasAgenda = Melhoria::getInstance()->order('prazo_legal')->order('prazo_acordado')->order('gut', 'desc')->order('id')->filtrarPorUrgencia([$urgenciasSelecionadas], ['*', '(coalesce(gravidade, 1) * coalesce(urgencia, 1) * coalesce(tendencia, 1)) as gut']);
 
 $gravidadesAll = Gravidade::getInstance()->order('id')->getAll();
 $urgenciasAll  = Urgencia::getInstance()->order('id')->getAll();
 $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
 
+$melhorias = [];
+
+foreach($melhoriasAgenda as $melhoriaAgenda) {
+
+    $prazoLegal = '';
+    if (!empty($melhoriaAgenda->prazo_legal)) {
+      list($anoPrazoLegal, $mesPrazoLegal, $diaPrazoLegal) = explode('-', $melhoriaAgenda->prazo_legal);
+      $prazoLegal = date('d/m/Y', mktime(0,0,0, $mesPrazoLegal, $diaPrazoLegal, $anoPrazoLegal));
+    }
+      
+    $prazoAcordado = '';
+    if (!empty($melhoriaAgenda->prazo_acordado)) {
+      list($anoPrazoAcordado, $mesPrazoAcordado, $diaPrazoAcordado) = explode('-', $melhoriaAgenda->prazo_acordado);
+      $prazoAcordado = date('d/m/Y', mktime(0,0,0, $mesPrazoAcordado, $diaPrazoAcordado, $anoPrazoAcordado));
+    }
+
+    $mesEntregaMelhoria = preg_replace('/\d{4}-(\d{2})-\d{2}/', "$1", $melhoriaAgenda->prazo_legal);
+
+    if(empty($mesEntregaMelhoria)) {
+      $mesEntregaMelhoria = preg_replace('/\d{4}-(\d{2})-\d{2}/', "$1", $melhoriaAgenda->prazo_acordado);
+    }
+    $mesEntregaMelhoria = (int)$mesEntregaMelhoria;
+  
+    $melhoriaAgenda->title      = preg_replace('/([^ ]+ +[^ ]+ +[^ ]+ +).*/', "$1", $melhoriaAgenda->tarefa);
+    $melhoriaAgenda->entrega_em = $mesEntregaMelhoria;
+    $melhoriaAgenda->prazoLegal    = $prazoLegal;
+    $melhoriaAgenda->prazoAcordado = $prazoAcordado;
+
+    $melhorias[$melhoriaAgenda->area][$mesEntregaMelhoria][$melhoriaAgenda->urgencia][] = $melhoriaAgenda;
+}
 ?>
 <div class="container-fluid">
 <table class="table table-striped">
@@ -57,36 +87,17 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
               <thead>
               </thead>
               <tbody>
-                <?php 
-                foreach($melhorias as $melhoria) {
-
-                    $title = preg_replace('/([^ ]+ +[^ ]+ +[^ ]+ +).*/', "$1", $melhoria->tarefa);
-
-                    $prazoLegal = '';
-                    if (!empty($melhoria->prazo_legal)) {
-                      list($anoPrazoLegal, $mesPrazoLegal, $diaPrazoLegal) = explode('-', $melhoria->prazo_legal);
-                      $prazoLegal = date('d/m/Y', mktime(0,0,0, $mesPrazoLegal, $diaPrazoLegal, $anoPrazoLegal));
-                    }
-                      
-                    $prazoAcordado = '';
-                    if (!empty($melhoria->prazo_acordado)) {
-                      list($anoPrazoAcordado, $mesPrazoAcordado, $diaPrazoAcordado) = explode('-', $melhoria->prazo_acordado);
-                      $prazoAcordado = date('d/m/Y', mktime(0,0,0, $mesPrazoAcordado, $diaPrazoAcordado, $anoPrazoAcordado));
-                    }
-
-                    $demandaLegal = (bool)$melhoria->demanda_legal ? true : false;
-
-                    if($melhoria->entrega_em == $m) {
-                ?>
                     <tr>
                         <?php foreach($urgencias as $urgencia) : ?>
-                            <?php if($melhoria->urgencia == $urgencia->id && $melhoria->area == $area->id) : ?>
+                    <?php $melhoriasEncontradas = !empty($melhorias[$area->id][$m][$urgencia->id]) ? $melhorias[$area->id][$m][$urgencia->id] : null; ?>
+                    <?php if(!empty($melhoriasEncontradas)) : ?>
+                      <?php foreach($melhoriasEncontradas as $melhoria) : ?>
                                 <td class="table-<?php echo $urgencia->id == 5 ? 'primary' : ($urgencia->id == 4 ? 'danger' : 'warning' ) ; ?>">
 
                                   <div class="wrapper-melhoria" id="wrapper_melhoria_<?php echo "{$area->id}_{$m}_{$melhoria->id}" ?>">
                                     <div class="card" style="width: 18rem;">
                                       <div class="card-body">
-                                        <h5 class="card-title"><?php echo $title; ?></h5>
+                                        <h5 class="card-title"><?php echo $melhoria->title; ?></h5>
                                         <p class="card-text"><?php echo $melhoria->tarefa ?></p>
                                         <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#melhoria_<?php echo $melhoria->id ?>">
                                           <i class="fas fa-plus"></i>
@@ -99,7 +110,7 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
                                       <div class="modal-dialog" role="document">
                                         <div class="modal-content">
                                           <div class="modal-header">
-                                            <h5 class="modal-title" id="melhoria_<?php echo $melhoria->tarefa ?>"><?php echo $title ?></h5>
+                                            <h5 class="modal-title" id="melhoria_<?php echo $melhoria->tarefa ?>"><?php echo $melhoria->title ?></h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                               <span aria-hidden="true">&times;</span>
                                             </button>
@@ -109,7 +120,7 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
                                               <div class="form-group">
                                                   <label class="" for="descricao">Descrição</label>
                                                   <textarea class="form-control mb-2" id="descricao" rows="8" readonly><?php echo $melhoria->descricao ?></textarea>
-                                              </div>
+                                                </div>
                                               <div class="form-group form-row">
                                                 <div class="col">
                                                   <label  for="prazo_legal">Prazo legal</label>
@@ -119,13 +130,13 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
                                                   <label for="prazo_acordado">Prazo acordado</label>
                                                   <input type="text" class="form-control" id="prazo_acordado" placeholder="Prazo acordado" readonly value="<?php echo $prazoAcordado ?>">
                                                 </div>
-                                              </div>
+                                                  </div>
                                               <div class="form-group form-row">
                                                 <div class="col">
                                                   <label for="gravidade">Gravidade</label>
                                                   <select class="form-control" id="gravidade">
+                                                    <option value='0'>Não informado</option>
                                                     <?php foreach ($gravidadesAll as $gravidade) : ?>
-                                                      <option value='0'>Não informado</option>
                                                       <option <?php echo $gravidade->id == $melhoria->gravidade ? 'selected' : '' ?> value="<?php echo $gravidade->id ?>"><?php echo $gravidade->descricao ?></option>
                                                     <?php endforeach; ?>
                                                   </select>
@@ -133,8 +144,8 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
                                                 <div class="col">
                                                   <label for="urgencia">Urgência</label>
                                                   <select class="form-control" id="urgencia">
+                                                    <option value='0'>Não informado</option>
                                                     <?php foreach ($urgenciasAll as $urgencia) : ?>
-                                                      <option value='0'>Não informado</option>
                                                       <option <?php echo $urgencia->id == $melhoria->urgencia ? 'selected' : '' ?> value="<?php echo $urgencia->id ?>"><?php echo $urgencia->descricao ?></option>
                                                     <?php endforeach; ?>
                                                   </select>
@@ -142,16 +153,17 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
                                               </div>
                                               <div class="form-group form-row">  
                                                 <div class="col">
+                                                  <label for="tendencia">Tendência</label>
                                                   <select class="form-control" id="tendencia">
+                                                    <option value='0'>Não informado</option>
                                                     <?php foreach ($tendenciasAll as $tendencia) : ?>
-                                                      <option value='0'>Não informado</option>
                                                       <option <?php echo $tendencia->id == $melhoria->tendencia ? 'selected' : '' ?> value="<?php echo $tendencia->id ?>"><?php echo $tendencia->descricao ?></option>
                                                     <?php endforeach; ?>
                                                   </select>
                                                 </div>
                                                 <div class="col">
                                                   <div class="form-check mb-2">
-                                                    <input class="form-check-input" type="checkbox" id="demanda_legal" readonly <?php echo $demandaLegal ? 'checked' : '' ?>>
+                                                    <input class="form-check-input" type="checkbox" id="demanda_legal" readonly <?php echo (bool)$melhoria->demanda_legal ? 'checked' : '' ?>>
                                                     <label class="form-check-label" for="demanda_legal">Demanda Legal</label>
                                                   </div>
                                                 </div>
@@ -167,13 +179,10 @@ $tendenciasAll = Tendencia::getInstance()->order('id')->getAll();
                                     </div>
                                   </div>
                                 </td>
+                      <?php endforeach; ?>
                             <?php endif; ?>
                         <?php endforeach; ?>
                     </tr>
-                <?php
-                    }
-                }
-                ?>
               </tbody>
             </table>
           </td>
