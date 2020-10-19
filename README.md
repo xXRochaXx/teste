3414204e8a604ac6223a147e5d61b5ce0869b3a5
Andrio <andrio.costa@dbseller.com.br>
2020-10-19 10:58:34 -0300

    atualizado documentação

--- README.md
+++ README.md
@@ -1,11 +1,70 @@
-# calendario-melhorias
+- [Projeto](#projeto)
+- [Deploy](#Deploy) 
+- [Deploy usando Docker](#deploy-docker)
+    - [Instalando Container / Servidor](#docker-servidor-up)
+    - [Banco de dados](#docker-banco)
+    - [Instalando dependências do projeto](#docker-dependencias-projeto)
+    - [Removendo os containers](#docker-remover)
+- [Deploy instalando servidor local ](#deploy-local)
+- [Considerações](#consideracoes)
 
-## Instalação
-```shell
-docker-compose up
+
+# Projeto
+Esse projeto é um teste que visa avaliar as habilidades do candidato em javascript, php e orientação a objetos.
+
+Baixe o projeto `calendario-melhorias` em seu computador conforme instruções do RH.
+
+## Deploy 
+
+Para facilitar o deploy da aplicação na sua maquina, disponibilizamos um servidor docker com todas as dependências necessárias.
+**Recomendamos que utilize o container docker**.
+
+Você pode optar também por levantar o projeto em sua maquina usando o seu servidor local se assim desejar.
+Para isso você precisa que seu servidor atendenda as depêndencias do projeto.
+
+<div id='deploy-docker'/>
+
+## Deploy por Docker
+Se você ainda não conhece o docker, vale a lida da [documentação oficial](https://docs.docker.com/).
+
+Se ainda não tem o docker instalado você precisa instalá-lo:
+ - [Linux](https://docs.docker.com/engine/install/)
+ - [Windows](https://docs.docker.com/docker-for-windows/install/)
+ - [Mac](https://docs.docker.com/docker-for-mac/install/)
+
+ Instale também o [docker-compose](https://docs.docker.com/compose/install/)
+
+<div id='docker-servidor-up'/>
+
+### Instalando Container / Servidor
+
+Primeiro você deve acessar o diretório do projeto `calendario-melhorias` como no exemplo:
+```
+/home/dbseller/calendario-melhorias/
 ```
 
-## Carregando dados na base
+Esse diretório possui o arquivo `docker-compose.yml`, é nele que fica as instruções para instalar/levantar o servidor. 
+
+
+Após instalar o docker, para levantar o servidor, você deve executar o comando através do seu terminal (terminal linux ou cmd no windows).
+
+```shell
+docker-compose up -d
+```
+
+Se for a primeira vez que você executou, o comando vai baixar todas as dependências para levantar o servidor e você terá que agurardar o download.
+
+Após instalado, o servidor levantará quase que instantaneamente. 
+
+**Dica**, após finalizar seu trabalho no dia, você pode parar o servidor atravéz do comando `docker-compose stop`. Mais lembre-se de no dia seguinte levantar o servidor novamente. Basta acessar o diretório do projeto e executar `docker-compose up -d`
+
+<div id='docker-banco'/>
+
+### Banco de dados
+O banco de dados esta dentro do container, para criar a base e inserir os dados iniciais, você precisa executar os seguintes comandos após levantar o servidor.
+
+> Para isso copie e cole os comandos abaixo no seu terminal linha a linha.
+
 ```shell
 docker exec -it dbsellerdb bash
 cd /home
@@ -14,7 +73,13 @@ psql -U postgres -f seeds/seeds.sql
 exit
 ```
 
-## Instalando dependências
+<div id='docker-dependencias-projeto'/>
+
+### Instalando dependências do projeto
+O servidor apache assim como o php estão dentro do container, e para instalar as dependências você também deve fazer através do container.
+
+> Para isso copie e cole os comandos abaixo no seu terminal linha a linha.
+
 ```shell
 docker exec -it dbsellerphp bash
 chmod +x composer
@@ -22,10 +87,66 @@ php composer install
 exit
 ```
 
-#### Correções de bugs e melhorias
-~~*Você deve criar um merge request com sua correção.*~~
-Crie um fork do projeto com as suas correções
+<div id='docker-remover'/>
 
-### ATENÇÃO
-O projeto não se trata de um projeto real, trata-se apenas de um teste, por isso solicito que não faça interações neste projeto. 
-Fork o projeto e para qualquer dúvida sore as tarefas recrie-as no seu projeto e comente lá.
+### Removendo os containers
+Após concluído o teste, você pode remover os containers através dos comandos:
+
+- Para os containers em execução: `docker stop $(docker ps -a -q)`
+- Deleta as imagens: `docker rmi -f $(docker images -q)`
+
+<div id='deploy-local'/>
+
+## Deploy instalando servidor local 
+
+Se você usa linux, já deve estar acostumado com a instalação de um servidor web compatível com a sua distribuição linux.
+
+Se você usa windows pode instalar o servidor utilizando XAMPP desde que atenda os requisitos abaixo. 
+
+Você deve consultar o oráculo, digo google, para instruções de instalação do servidor local de acordo com seu OS.
+
+Requisitos de servidor: 
+
+- Servidor apache
+- Postegres >= 9.4  
+- PHP 5.6
+
+PHP Extensions
+- pdo_pgsql
+- pgsql
+- opcache
+- zip
+- bcmath
+- bz2
+- gd
+- json
+- mbstring
+- mcrypt
+- xml
+- xmlrpc
+
+Mais informações sobre as extensões do php, segue o [link](https://www.php.net/manual/pt_BR/extensions.alphabetical.php) da documentação.
+
+
+Após instalar e configurar você deve criar o banco de dados. 
+Para isso você deve logar no postgres e:
+- criar um banco chamado `melhorias`
+- rodar o script que estar no path `calendario-melhorias/database/database-local.sql`
+
+
+> **ATENÇÃO** As suas correções/implementações devem funcionar de acordo com os requisitos do servidor, pois o projeto será avaliado em um servidor com essas dependências. Por isso recomendamos utilizar o container disponibilizado com o projeto. 
+
+
+<div id='consideracoes'/>
+
+## Considerações
+Conforme instrução do RH, você deve criar um fork do projeto.
+
+Em seu fork corrija os bugs reportado e implemente as melhorias propostas.
+
+**Atenção** Não esqueça de comitar e dar push para o servidor as suas correções.
+
+Desejamos a você um bom teste!
+
+> O projeto não se trata de um projeto real, trata-se apenas de um teste, por isso solicitamos que não faça interações neste projeto.
+Fork o projeto e para qualquer dúvida sobre as tarefas recrie-as no seu projeto e comente lá.
---
+++ database/database-local.sql
@@ -0,0 +1,100 @@
+
+CREATE SCHEMA config AUTHORIZATION postgres;
+GRANT ALL ON SCHEMA config TO postgres;
+
+CREATE TABLE config.gravidade (
+    id  serial primary key,
+    descricao varchar(250)
+);
+
+INSERT INTO config.gravidade (descricao) VALUES ('Sem gravidade'), ('Pouco grave'), ('Grave'), ('Muito grave'), ('Extremamente grave');
+
+CREATE TABLE config.urgencia (
+    id  serial primary key,
+    descricao varchar(250)
+);
+INSERT INTO config.urgencia (descricao) VALUES ('Pode esperar'), ('Pouco urgente'), ('Merece atenção, curto prazo'), ('Muito urgente'), ('Precisa de ação imediata');
+
+CREATE TABLE config.tendencia (
+    id  serial primary key,
+    descricao varchar(250)
+);
+INSERT INTO config.tendencia (descricao) VALUES ('Não irá mudar'), ('Piora em longo prazo'), ('Piora em médio prazo'), ('Piora em curto prazo'), ('Piora rapidamente');
+
+CREATE TABLE area (
+    id serial primary key,
+    descricao varchar(250)
+);
+
+CREATE TABLE melhorias (
+    id  serial primary key,
+    tarefa varchar(250),
+    descricao text not null,
+    demanda_legal boolean not null default false,
+    prazo_acordado date,
+    prazo_legal date,
+    gravidade int8,
+    urgencia int8, 
+    tendencia int8,
+    area int8 NOT NULL
+);
+
+ALTER TABLE melhorias ADD CONSTRAINT melhoria_gravidade_fk FOREIGN KEY (gravidade) REFERENCES config.gravidade (id);
+ALTER TABLE melhorias ADD CONSTRAINT melhoria_urgencia_fk FOREIGN KEY (urgencia) REFERENCES config.urgencia (id);
+ALTER TABLE melhorias ADD CONSTRAINT melhoria_tendencia_fk FOREIGN KEY (tendencia) REFERENCES config.tendencia (id);
+ALTER TABLE melhorias ADD CONSTRAINT melhoria_area_fk FOREIGN KEY (area) references area (id);
+
+
+
+INSERT INTO area (descricao) VALUES 
+     ('Configurações')
+    ,('Educação')
+    ,('Financeiro')
+    ,('Patrimonial')
+    ,('Saúde')
+    ,('Tributário')
+    ,('Folha')
+;
+
+INSERT INTO melhorias (area, descricao, prazo_acordado, prazo_legal, demanda_legal, gravidade, urgencia, tendencia) VALUES 
+    ((SELECT id FROM area WHERE descricao ilike 'Tributário'),     'A equipe de suporte precisa saber que o deploy automatizado no Heroku facilitou a resolução de conflito da execução parelela de funções em multi-threads.', '2019-12-31', null,         'f', null, 1, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Tributário'),     'Nesse pull request, um erro não identificado otimizou a renderização na organização alfanumérico dos arrays multidimensionais', '2019-04-30', null,         'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Tributário'),     'Desde ontem a noite o gerenciador de dependências do frontend corrigiu o bug da execução parelela de funções em multi-threads.', '2019-05-31', null,         'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Tributário'),     'Fala pro cliente que a disposição dos elementos HTML deletou todas as entradas do fluxo de dados de forma retroativa no server.', '2019-04-30', null,         'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Tributário'),     'Explica pro Product Onwer que a disposição dos elementos HTML complexificou o merge no fechamento automático das tags.', '2019-08-28', null,         'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Saúde'),          'Dado o fluxo de dados atual, a normalização da data facilitou a resolução de conflito na definição de variaveis com tipos estáticos.', '2019-08-28', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Saúde'),          'Fala pro cliente que o gerenciador de dependências do frontend causou a race condition do carregamento de JSON delimitado por linhas.', '2019-03-28', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Saúde'),          'Desde ontem a noite um erro não identificado deletou todas as entradas na organização alfanumérico dos arrays multidimensionais', '2019-12-31', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Saúde'),          'Explica pro Product Onwer que o último pull request desse SCRUM causou o bug do polimorfismo aplicado nas classes.', null,         '2019-05-31', 'f', null, 3, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Saúde'),          'Nesse pull request, a otimização de performance da renderização do DOM deletou todas as entradas do polimorfismo aplicado nas classe', null,         '2019-06-30', 'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Patrimonial'),    'Desde ontem a noite o módulo de recursão paralela causou a race condition da execução parelela de funções em multi-threads.', null,         '2019-05-02', 'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Patrimonial'),    'Explica pro Product Onwer que o deploy automatizado no Heroku superou o desempenho na organização alfanumérico dos arrays multidimensionais', null,         '2019-05-30', 'f', null, 1, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Patrimonial'),    'Nesse pull request, o gerenciador de dependências do frontend otimizou a renderização do polimorfismo aplicado nas classes.', '2019-05-31', null,         't', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Patrimonial'),    'Com este commit, a compilação final do programa complexificou o merge do carregamento de JSON delimitado por linhas.', '2019-12-31', null,         't', null, 1, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Patrimonial'),    'A equipe de suporte precisa saber que a normalização da data facilitou a resolução de conflito do fluxo de dados de forma retroativa no server.', '2019-04-30', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Folha'),          'Desde ontem a noite o deploy automatizado no Heroku superou o desempenho de uma configuração Webpack eficiente nos builds.', '2019-05-31', null,         't', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Folha'),          'A equipe de suporte precisa saber que um erro não identificado causou o bug da execução de requisições effcientes na API.', '2019-04-30', null,         'f', null, 3, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Folha'),          'Nesse pull request, a otimização de performance da renderização do DOM causou o bug no fechamento automático das tags.', '2019-08-28', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Folha'),          'Explica pro Product Onwer que a otimização de performance da renderização do DOM otimizou a renderização no parse retroativo do DOM.', '2019-08-28', null,         'f', null, 3, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Folha'),          'Fala pro cliente que o deploy automatizado no Heroku causou o bug na estabilidade do protocolo de transferência de dados.', '2019-03-28', null,         'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Financeiro'),     'Com este commit, o módulo de recursão paralela otimizou a renderização de uma compilação com tempo acima da media.', '2019-12-31', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Financeiro'),     'Nesse pull request, a compilação final do programa causou a race condition na estabilidade do protocolo de transferência de dados.', '2019-05-31', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Financeiro'),     'Desde ontem a noite o último pull request desse SCRUM complexificou o merge do JSON compilado a partir de proto-buffers.', '2019-06-30', null,         'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Financeiro'),     'Fala pro cliente que a compilação final do programa facilitou a resolução de conflito na organização alfanumérico dos arrays multidimensionais', null,         '2019-05-02', 'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Financeiro'),     'Dado o fluxo de dados atual, o gerenciador de dependências do frontend corrigiu o bug na compilação de templates literais.', null,         '2019-05-30', 'f', null, 4, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Educação'),       'Nesse pull request, o deploy automatizado no Heroku causou a race condition da renderização de floats parciais.', null,         '2019-05-31', 'f', null, 2, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Educação'),       'A equipe de suporte precisa saber que a disposição dos elementos HTML superou o desempenho na criação de novos polyfills para suportar os processos.', null,         '2019-08-28', 'f', null, 3, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Educação'),       'Desde ontem a noite a otimização de performance da renderização do DOM otimizou a renderização do fluxo de dados de forma retroativa no server.', '2019-08-28', null,         't', null, 3, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Educação'),       'Com este commit, a compilação final do programa causou o bug na compilação de templates literais.', '2019-03-28', null,         't', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Educação'),       'Fala pro cliente que o gerenciador de dependências do frontend deletou todas as entradas na definição de variaveis com tipos estáticos.', null,         '2019-12-31', 't', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Configurações'),  'Explica pro Product Onwer que a normalização da data deletou todas as entradas dos parametros passados em funções privadas.', null,         '2019-05-31', 'f', null, 1, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Configurações'),  'Nesse pull request, o módulo de recursão paralela facilitou a resolução de conflito de uma compilação com tempo acima da media.', null,         '2019-06-30', 'f', null, 1, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Configurações'),  'Explica pro Product Onwer que a normalização da data otimizou a renderização no fechamento automático das tags.', '2019-05-02', null,         'f', null, 1, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Configurações'),  'Fala pro cliente que a normalização da data otimizou a renderização da execução parelela de funções em multi-threads.', '2019-05-30', null,         'f', null, 5, null),
+    ((SELECT id FROM area WHERE descricao ilike 'Configurações'),  'A equipe de suporte precisa saber que a compilação final do programa complexificou o merge no fechamento automático das tags.', '2019-05-31', null,         'f', null, 5, null)
+;
+
+
+UPDATE melhorias SET descricao = replace(descricao, '\n', chr(10)) WHERE id IN (SELECT id FROM melhorias WHERE strpos(descricao, '\n') > 0);
+UPDATE melhorias SET tarefa = replace(substr(descricao, 0, 100), chr(10), ' ') ;
+UPDATE melhorias SET tarefa = tarefa || '...' WHERE length(replace(descricao, chr(10), ' ')) > 100;
